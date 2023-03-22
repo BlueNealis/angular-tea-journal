5b4abca8d873221d15cdd60ee977453b3f7ae4d5
Blue Jessen <canealisblue@gmail.com>
2023-03-22 15:10:26 -0700

    Add some styling to tea card container

--- src/app/tea-list/tea-detail/tea-detail.component.ts
+++ src/app/tea-list/tea-detail/tea-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tea-detail',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./tea-detail.component.css']
 })
 export class TeaDetailComponent {
-
+addToFavorites() {
+  console.log("favorited")
+}
 }
