31cacd8570a22c3581a0f27f606e51efc0a05c2c
Blue Jessen <canealisblue@gmail.com>
2023-03-22 11:17:39 -0700

    Add new components and initialize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeaListComponent } from './tea-list/tea-list.component';
+import { TeaDetailComponent } from './tea-list/tea-detail/tea-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeaListComponent,
+    TeaDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tea-list/tea-detail/tea-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tea-detail',
+  templateUrl: './tea-detail.component.html',
+  styleUrls: ['./tea-detail.component.css']
+})
+export class TeaDetailComponent {
+
+}
